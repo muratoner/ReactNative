8f663e25811db81ac251e529bcdd94dc203db248
Murat ÖNER <info@muratoner.net>
2018-12-25 19:23:46 +0300

    Added new VM file.

---
+++ react-native-dotnetify/server/Hubs/LiveListVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using DotNetify;
+
+namespace dotnetify.Hubs
+{
+    public class LiveListVM : BaseVM
+    {
+        /*
+         * Her 1sn'yede bir timer nesnesinin çalışması için 1000ms değerini burada belirliyoruz ve bu field'ı LiveListVM contructor'ında yer alan timer nesnesi constructor'ında kullanıyoruz.
+         */
+        private const int _timeInterval = 1000;
+
+        /*
+         * React-native tarafındaki People dizimizi beslemek için fake olarak data üreteceğimiz timer nesnemiz.
+         */
+        private Timer _timer;
+
+        public LiveListVM()
+        {
+            _timer = new Timer(state =>
+            {
+                // React Native tarafında state nesnemiz içerisindeki people dizisine yeni kaydın eklenmesini sağlıyoruz.
+                this.AddList("people", new Person
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = Faker.Name.First(),
+                    Surname = Faker.Name.Last(),
+                    Age = Faker.RandomNumber.Next(18, 60)
+                });
+                // PushUpdates ile yapılan değişikliklerin bu LiveGaugeVM'i kullanan aktif ilgili ekranlara yansıması sağlanıyor.
+                PushUpdates(); 
+            }, null, _timeInterval, _timeInterval);
+        }
+
+        public override void Dispose()
+        {
+            _timer.Dispose();
+            base.Dispose();   
+        }
+    }
+}
---
+++ react-native-dotnetify/server/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnetify.Hubs
+{
+    internal class Person
+    {
+        public string Name { get; internal set; }
+        public string Surname { get; internal set; }
+        public int Age { get; internal set; }
+        public string Id { get; internal set; }
+    }
+}
