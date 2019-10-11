a09480d90c0f2596d73a2f3522720c700a9b7567
Murat ÖNER <info@muratoner.net>
2019-10-11 22:58:12 +0300

    added classic state management.

--- redux/basic/App.tsx
+++ redux/basic/App.tsx
@@ -18,7 +18,22 @@ import {
 class App extends Component {
 
 	constructor() {
-		super()
+		super()		
+		this.state = {
+			count: 0
+		}
+	}	
+
+	increase = () => {
+		this.setState({
+			count: this.state.count + 1	
+		})
+	}
+
+	decrease = () => {
+		this.setState({
+			count: this.state.count - 1	
+		})
 	}
 
 	render() {
@@ -31,7 +46,18 @@ class App extends Component {
 						style={styles.scrollView}>
 						<Header />
 						<View style={styles.body}>
-							
+							{/* Classic Technic For State Management */}
+							<View style={{ flexDirection: 'row', justifyContent: 'space-around', width: 200, alignSelf: 'center', marginTop: 50 }}>
+								<TouchableOpacity onPress={() => this.increase()}>
+									<Text style={{ fontSize: 20 }}>Increase</Text>
+								</TouchableOpacity>
+								<Text style={{ fontSize: 20 }}>
+									{this.state.count}
+								</Text>
+								<TouchableOpacity onPress={() => this.decrease()}>
+									<Text style={{ fontSize: 20 }}>Descrease</Text>
+								</TouchableOpacity>
+							</View>
 						</View>
 					</ScrollView>
 				</SafeAreaView>
