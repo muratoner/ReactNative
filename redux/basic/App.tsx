173fde1e075029cae8b7c99cf4c6abf3fa922760
Murat ÖNER <info@muratoner.net>
2019-10-11 22:55:46 +0300

    cleaned some code blocks.

--- redux/basic/App.tsx
+++ redux/basic/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { Component } from 'react';
 import {
 	SafeAreaView,
@@ -16,16 +6,13 @@ import {
 	View,
 	Text,
 	StatusBar,
-	TouchableHighlight,
 	TouchableOpacity,
 } from 'react-native';
 
 import {
 	Header,
 	LearnMoreLinks,
-	Colors,
-	DebugInstructions,
-	ReloadInstructions,
+	Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 class App extends Component {
@@ -57,39 +44,9 @@ const styles = StyleSheet.create({
 	scrollView: {
 		backgroundColor: Colors.lighter,
 	},
-	engine: {
-		position: 'absolute',
-		right: 0,
-	},
 	body: {
 		backgroundColor: Colors.white,
-	},
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: '600',
-		color: Colors.black,
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: '400',
-		color: Colors.dark,
-	},
-	highlight: {
-		fontWeight: '700',
-	},
-	footer: {
-		color: Colors.dark,
-		fontSize: 12,
-		fontWeight: '600',
-		padding: 4,
-		paddingRight: 12,
-		textAlign: 'right',
-	},
+	}
 });
 
 export default App;
