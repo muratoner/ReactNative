f2320c79aecd84ef4d50d2ae1515c7e24d7c8fee
Murat ÖNER <info@muratoner.net>
2019-10-11 23:16:40 +0300

    basic redux process.

--- redux/basic/App.tsx
+++ redux/basic/App.tsx
@@ -1,78 +1,30 @@
 import React, { Component } from 'react';
-import {
-	SafeAreaView,
-	StyleSheet,
-	ScrollView,
-	View,
-	Text,
-	StatusBar,
-	TouchableOpacity,
-} from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CounterApp from './src/CounterApp';
 
-import {
-	Header,
-	LearnMoreLinks,
-	Colors
-} from 'react-native/Libraries/NewAppScreen';
+const initialState = {
+	count: 0
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INCREASE_COUNT':
+			return { count: state.count + 1 }
+		case 'DECREASE_COUNT':
+			return { count: state.count - 1 }
+	}
+	return state
+}
+
+const store = createStore(reducer)
 
 class App extends Component {
-
-	constructor() {
-		super()		
-		this.state = {
-			count: 0
-		}
-	}	
-
-	increase = () => {
-		this.setState({
-			count: this.state.count + 1	
-		})
-	}
-
-	decrease = () => {
-		this.setState({
-			count: this.state.count - 1	
-		})
-	}
-
 	render() {
-		return (
-			<>
-				<StatusBar barStyle="dark-content" />
-				<SafeAreaView>
-					<ScrollView
-						contentInsetAdjustmentBehavior="automatic"
-						style={styles.scrollView}>
-						<Header />
-						<View style={styles.body}>
-							{/* Classic Technic For State Management */}
-							<View style={{ flexDirection: 'row', justifyContent: 'space-around', width: 200, alignSelf: 'center', marginTop: 50 }}>
-								<TouchableOpacity onPress={() => this.increase()}>
-									<Text style={{ fontSize: 20 }}>Increase</Text>
-								</TouchableOpacity>
-								<Text style={{ fontSize: 20 }}>
-									{this.state.count}
-								</Text>
-								<TouchableOpacity onPress={() => this.decrease()}>
-									<Text style={{ fontSize: 20 }}>Descrease</Text>
-								</TouchableOpacity>
-							</View>
-						</View>
-					</ScrollView>
-				</SafeAreaView>
-			</>
-		);
+		return <Provider store={store}>
+			<CounterApp />
+		</Provider>
 	}
 };
-
-const styles = StyleSheet.create({
-	scrollView: {
-		backgroundColor: Colors.lighter,
-	},
-	body: {
-		backgroundColor: Colors.white,
-	}
-});
 
 export default App;
